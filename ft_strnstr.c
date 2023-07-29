bbea348137da39681d5aebdebe3164aafb7b95ac
José Felipe <correio@josefelipe.eng.br>
2023-07-29 17:45:24 -0300

    new file:   .gitignore 	modified:   ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,11 +6,11 @@
 /*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 14:48:37 by josfelip          #+#    #+#             */
-/*   Updated: 2023/07/27 16:01:36 by josfelip         ###   ########.fr       */
+/*   Updated: 2023/07/29 17:25:58 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h>
+#include "libft.h"
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
